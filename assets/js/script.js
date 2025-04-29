c9f2d7d3d4a5bf3be1dac225d3c32be1ee840f40
marischi <marileneifsp@gmail.com>
2025-04-29 17:37:38 -0300

    Busca

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,43 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     const dropdownToggle = document.querySelectorAll('[data-dropdown-toggle]');
     dropdownToggle.forEach((toggle) => {
-        toggle.addEventListener('click', function() {
+        toggle.addEventListener('click', function () {
             const parent = this.closest('.dropdown-item');
             console.log('teste');
             parent.classList.toggle('active');
         })
     })
+    //Fechar o dropdown ao clicar fora
+    document.addEventListener('click', function (e) {
+        dropdownToggle.forEach((toggle) => {
+            const parent = toggle.closest('.dropdown-item');
+            if (!parent.contains(e.target)) {
+                parent.classList.remove('active');
+            }
+        })
+    })
+})
 
+const searchToggle = document.getElementById('searchToggle');
+const searchOverlay = document.getElementById('searchOverlay');
+const closeSearch = document.getElementById('closeSearch');
 
+searchToggle.addEventListener('click', () => {
+    searchOverlay.style.display = 'flex';
+    setTimeout(() => {
+        searchOverlay.querySelector('.search-input').focus();
+    }, 100);
+});
+
+//fechar o botão
+
+closeSearch.addEventListener('click', () => {
+    searchOverlay.style.display = 'none';
+})
+
+//fechar com o ESC
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        searchOverlay.style.display = 'none';
+    }
 })
