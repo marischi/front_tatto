9d64294480aa57a18f44cca567c59e1233536cc3
marischi <marileneifsp@gmail.com>
2025-05-06 17:52:04 -0300

    galeria de imagens

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,3 +41,20 @@ document.addEventListener('keydown', (e) => {
         searchOverlay.style.display = 'none';
     }
 })
+
+//Galeria
+const imagens = document.querySelectorAll('.galeria img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+
+imagens.forEach((img) => {
+    img.addEventListener('click', () => {
+        lightboxImg.src = img.src;
+        lightbox.classList.add('show');
+    })    
+})
+
+//esconder o lightbox
+lightbox.addEventListener('click', () => {
+    lightbox.classList.remove('show');
+})
